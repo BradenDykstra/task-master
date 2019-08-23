dc35aca9e25feb6e0d50772de0c6e98da3a71df8
Braden Dykstra <Toastie285@gmail.com>
2019-08-23 15:54:12 -0600

    window

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,5 +1,3 @@
-//FIXME 404 on live version?
-
 export default class List {
     //TODO You will need to create a constructor 
     //and the methods needed to create the view template for this model
